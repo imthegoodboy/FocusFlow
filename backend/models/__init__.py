0cfef8eee50aa3d9344f1fc9789a0f307329f79a
Nikku <nikku696969@gmail.com>
2025-11-21 12:58:24 +0530

    updating

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,10 +1,23 @@
-from .user import User, UserCreate, UserLogin, UserResponse, StudentProfile, SurveyResponse
+from .user import User, UserCreate, UserLogin, UserResponse
 from .task import Task, TaskCreate, TaskUpdate, TaskResponse
 from .routine import RoutineLog, RoutineLogCreate, RoutineLogResponse
+from .student import StudentProfile, StudentProfileCreate, StudentProfileResponse, SurveyAnswers
 
 __all__ = [
-    "User", "UserCreate", "UserLogin", "UserResponse", "StudentProfile", "SurveyResponse",
-    "Task", "TaskCreate", "TaskUpdate", "TaskResponse",
-    "RoutineLog", "RoutineLogCreate", "RoutineLogResponse"
+    "User",
+    "UserCreate",
+    "UserLogin",
+    "UserResponse",
+    "StudentProfile",
+    "StudentProfileCreate",
+    "StudentProfileResponse",
+    "SurveyAnswers",
+    "Task",
+    "TaskCreate",
+    "TaskUpdate",
+    "TaskResponse",
+    "RoutineLog",
+    "RoutineLogCreate",
+    "RoutineLogResponse",
 ]
 
